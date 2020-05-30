109ba3d3245aa78eb8c13b17f89c6b5e7994c053
Eduardo Arruda <caduknebel@gmail.com>
2020-05-30 10:17:54 -0300

    feat: Cria seeder e factory de produtos

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'sku' => $faker->swiftBicNumber(),
+        'title' => $faker->sentence(3),
+        'price' => $faker->randomFloat(2, 0, 1000),
+        'stock' => $faker->numberBetween(0, 500)
+    ];
+});
