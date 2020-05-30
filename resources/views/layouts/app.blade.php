0c19e0894da54f0983a5ed1afa357b33b2949912
Eduardo Arruda <caduknebel@gmail.com>
2020-05-30 11:22:42 -0300

    style: Active dinamico na navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,13 +25,13 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
-                        <li class="nav-item active">
+                        <li class="nav-item {{ (request()->is('home')) ? 'active' : '' }}">
                             <a class="nav-link" href="{{ route('home') }}">Dashboard</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item {{ (request()->is('products*')) ? 'active' : '' }}">
                             <a class="nav-link" href="{{ route('products.index') }}">Produtos</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item {{ (request()->is('stock*')) ? 'active' : '' }}">
                             <a class="nav-link" href="#">Estoque</a>
                         </li>
                     </ul>
